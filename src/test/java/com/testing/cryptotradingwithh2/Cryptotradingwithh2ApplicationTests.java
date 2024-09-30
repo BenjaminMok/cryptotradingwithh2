952fcfd1167e45d9b9035efec2f7b9e062fe7e89
BenjaminMok <benjaminbocha@gmail.com>
2024-10-01 02:13:15 +0800

    Initial Commit
    Empty spring boot project

---
+++ src/test/java/com/testing/cryptotradingwithh2/Cryptotradingwithh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.cryptotradingwithh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cryptotradingwithh2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
